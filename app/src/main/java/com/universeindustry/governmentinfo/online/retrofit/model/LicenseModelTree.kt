263368af9b24f913e568a187b06df8b46e8adf4f
juyeon <benedictarthur1026@gmail.com>
2021-01-19 05:37:10 +0900

    #8 'license' model error

--- app/src/main/java/com/universeindustry/governmentinfo/online/retrofit/model/LicenseModelTree.kt
+++ app/src/main/java/com/universeindustry/governmentinfo/online/retrofit/model/LicenseModelTree.kt
@@ -1,64 +1,62 @@
 package com.universeindustry.governmentinfo.online.retrofit.model
 
-import com.tickaroo.tikxml.annotation.PropertyElement
-import com.tickaroo.tikxml.annotation.Xml
-import com.universeindustry.governmentinfo.views.fragments.license.LicenseModel
 import org.simpleframework.xml.Element
 import org.simpleframework.xml.ElementList
+import org.simpleframework.xml.Path
 import org.simpleframework.xml.Root
 
 @Root(name = "items", strict = false)
-data class Items (
+data class Items @JvmOverloads constructor (
         /*
         inline
         =  you're specifying that the elements you want parsed are in the same parent tree of a target element
         =  구문 분석할 요소가 대상 요소의 동일한 상위 트리에 있는지 지정.
          */
-        @field:ElementList(inline = true, required = false)
+        @ElementList(inline = true, name = "item", required = false)
 //        @param:ElementList(inline = true, name = "item")
         val itemList: MutableList<Item>? = null
 
 )
 
 @Root(name = "item", strict = false)
-data class Item  (
-        @field:Element(name = "jmcd", required = false)
+data class Item @JvmOverloads constructor  (
+        @Element(name = "jmcd", required = false)
 //        @param:Element(name = "jmcd", required = false)
         val jmcd: Int = 0,
 
-        @field:Element(name = "jmfldnm", required = false)
+        @Element(name = "jmfldnm", required = false)
 //        @param:Element(name = "jmfldnm", required = false)
         val jmfldnm: String? = null,
 
-        @field:Element(name = "mdobligfldcd", required = false)
+        @Element(name = "mdobligfldcd", required = false)
 //        @param:Element(name = "mdobligfldcd", required = false)
         val mdobligfldcd: Int = 0,
 
-        @field:Element(name = "mdobligfldnm", required = false)
+        @Element(name = "mdobligfldnm", required = false)
 //        @param:Element(name = "mdobligfldnm", required = false)
         val mdobligfldnm: String? = null,
 
-        @field:Element(name = "obligfldcd", required = false)
+        @Element(name = "obligfldcd", required = false)
 //        @param:Element(name = "obligfldcd", required = false)
         val obligfldcd: Int = 0,
 
-        @field:Element(name = "obligfldnm", required = false)
+        @Element(name = "obligfldnm", required = false)
 //        @param:Element(name = "obligfldnm", required = false)
         val obligfldnm: String? = null,
 
-        @field:Element(name = "qualgbcd", required = false)
+        @Element(name = "qualgbcd", required = false)
 //        @param:Element(name = "qualgbcd", required = false)
         val qualgbcd: Int = 0,
 
-        @field:Element(name = "qualgbnm", required = false)
+        @Element(name = "qualgbnm", required = false)
 //        @param:Element(name = "qualgbnm", required = false)
         val qualgbnm: String? = null,
 
-        @field:Element(name = "seriescd", required = false)
+        @Element(name = "seriescd", required = false)
 //        @param:Element(name = "seriescd", required = false)
         val seriescd: String? = null,
 
-        @field:Element(name = "seriesnm", required = false)
+        @Element(name = "seriesnm", required = false)
 //        @param:Element(name = "seriesnm", required = false)
         val seriesnm: String? = null
 )
