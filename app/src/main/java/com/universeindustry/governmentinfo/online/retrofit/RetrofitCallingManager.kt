5ee522478d48e33a0275b8031cd96100abda53a3
juyeon <benedictarthur1026@gmail.com>
2021-01-30 11:23:52 +0900

    #17 Bank desposit API model response update

--- app/src/main/java/com/universeindustry/governmentinfo/online/retrofit/RetrofitCallingManager.kt
+++ app/src/main/java/com/universeindustry/governmentinfo/online/retrofit/RetrofitCallingManager.kt
@@ -1,11 +1,9 @@
 package com.universeindustry.governmentinfo.online.retrofit
 
 import android.util.Log.e
-import com.google.gson.JsonArray
 import com.google.gson.JsonElement
-import com.google.gson.JsonObject
 import com.universeindustry.governmentinfo.online.retrofit.model.BankDespositModelTree
-import org.json.JSONObject
+import com.universeindustry.governmentinfo.online.retrofit.model.OptionListItem
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -36,37 +34,67 @@ class RetrofitCallingManager {
                                 val despositList : ArrayList<BankDespositModelTree> = arrayListOf()
                                 body.getAsJsonArray("baseList").forEach {
                                     val data =  it.asJsonObject
-                                    val itemTitle = data.get("fin_prdt_nm").asString // 상품이름
-                                    val bankTitle = data.get("kor_co_nm").asString // 은행이름
-                                    val code = data.get("fin_prdt_cd").asString // 금융상품 코드
+                                    val dcls_month = data.get("dcls_month").asString // 공시 제출월
+                                    val fin_co_no = data.get("fin_co_no").asString // 금융회사 코드
+                                    val fin_prdt_cd = data.get("fin_prdt_cd").asString // 상품이름
+                                    val kor_co_nm = data.get("kor_co_nm").asString // 금융상품 코드
+                                    val fin_prdt_nm = data.get("fin_prdt_nm").asString // 금융 상품명
+                                    val join_way = data.get("join_way").asString // 가입 방법
+                                    val mtrt_int = data.get("mtrt_int").asString // 만기후 이자율
+                                    val spcl_cnd = data.get("spcl_cnd").asString // 우대조건
+                                    val join_deny = data.get("join_deny").asString // 가입제한
+                                    val join_member = data.get("join_member").asString // 가입대상
+                                    val etc_note = data.get("etc_note").asString // 기타 유의사항
+                                    val max_limit = data.get("max_limit").toString() // 최고한도
+                                    val dcls_strt_day = data.get("dcls_strt_day").asString // 공시 시작일
+                                    val dcls_end_day = data.get("dcls_end_day").toString() // 공시 종료일
+                                    val fin_co_subm_day = data.get("fin_co_subm_day").asString // 금융회사 제출일
 
                                     val despositItem = BankDespositModelTree(
-                                            itemTItle = itemTitle,
-                                            bankTitle = bankTitle,
-                                            type = null,
-                                            saving = null,
-                                            vip = null,
-                                            term = null,
-                                            code = code
+                                            dcls_month = dcls_month,
+                                            fin_co_no = fin_co_no,
+                                            kor_co_nm = kor_co_nm,
+                                            fin_prdt_cd = fin_prdt_cd,
+                                            fin_prdt_nm = fin_prdt_nm,
+                                            join_way = join_way,
+                                            mtrt_int = mtrt_int,
+                                            spcl_cnd = spcl_cnd,
+                                            join_deny = join_deny,
+                                            join_member = join_member,
+                                            etc_note = etc_note,
+                                            max_limit = max_limit,
+                                            dcls_strt_day = dcls_strt_day,
+                                            dcls_end_day = dcls_end_day,
+                                            fin_co_subm_day = fin_co_subm_day,
+                                            optionListItem = arrayListOf()
                                     )
                                     despositList.add(despositItem)
                                 }
+
+                                val optionList = body.getAsJsonArray("optionList")
+                                optionList.forEachIndexed { index, jsonElement ->
+                                    val data =  jsonElement.asJsonObject
+                                    val fin_prdt_cd = data.get("fin_prdt_cd").asString // 금융상품 코드
+                                    val intr_rate_type = data.get("intr_rate_type").asString // 저축 금리 유형
+                                    val intr_rate_type_nm = data.get("intr_rate_type_nm").asString // 저축 금리 유형명
+                                    val save_trm = data.get("save_trm").asString // 저축 기간 (단위 : 개월)
+                                    val intr_rate = data.get("intr_rate").asString // 저축 금리 (소수점 2자리)
+                                    val intr_rate2 = data.get("intr_rate2").asString // 최고 우대금리 (소수점 2자리)
+
+                                    for (i in despositList.indices){
+                                        if (despositList[i].fin_prdt_cd == fin_prdt_cd){
+                                            val optionListItem = OptionListItem(
+                                                    intr_rate_type = intr_rate_type,
+                                                    intr_rate_type_nm = intr_rate_type_nm,
+                                                    save_trm = save_trm,
+                                                    intr_rate = intr_rate,
+                                                    intr_rate2 = intr_rate2
+                                            )
+                                            despositList[i].optionListItem.add(optionListItem)
+                                        }
+                                    }
+                                }
                                 completion(despositList)
-
-//                                body.getAsJsonArray("optionList").forEach {
-//                                    val data =  it.asJsonObject
-//                                    val type = data.get("intr_rate_type_nm").asString // 저축 금리 유형
-//                                    val term = data.get("save_trm").asString // 저축 기간
-//                                    val saving = data.get("intr_rate").asString // 기본 저축금리
-//                                    val vip = data.get("intr_rate2").asString // 최고 우대금리
-//                                    val code = data.get("fin_prdt_cd").asString // 금융상품 코드
-//
-//
-//                                }
-                            }
-                            "일일검색 허용횟수 초과" -> {
-                                // 개인의 경우로, 일일허용횟수를 초과하여 사용한 경우
-
                             }
                             else -> {
                                 // Open API 서비스 내부 시스템 에러
--- app/src/main/java/com/universeindustry/governmentinfo/online/retrofit/model/BankDespositModelTree.kt
+++ app/src/main/java/com/universeindustry/governmentinfo/online/retrofit/model/BankDespositModelTree.kt
@@ -3,22 +3,60 @@ package com.universeindustry.governmentinfo.online.retrofit.model
 import java.io.Serializable
 
 data class BankDespositModelTree(
-        val itemTItle : String?,
-        val bankTitle : String,
-        val type : String?,
-        val saving : String?,
-        val vip : String?,
-        val term : String?,
-        val code : String?
-) : Serializable
+        val dcls_month : String?,           // 공시 제출월
+        val fin_co_no : String?,            // 금융회사 코드
+        val kor_co_nm : String?,            // 금융회사명
+        val fin_prdt_cd : String?,          // 금융상품 코드
+        val fin_prdt_nm : String?,          // 금융 상품명
+        val join_way : String?,             // 가입 방법
+        val mtrt_int : String?,             // 만기후 이자율
+        val spcl_cnd : String?,             // 우대조건
+        val join_deny : String?,            // 가입제한
+        val join_member : String?,          // 가입대상
+        val etc_note : String?,             // 기타 유의사항
+        val max_limit : String?,            // 최고한도
+        val dcls_strt_day : String?,        // 공시 시작일
+        val dcls_end_day : String?,         // 공시 종료일
+        val fin_co_subm_day : String?,      // 금융회사 제출일
+        var optionListItem : ArrayList<OptionListItem?> // 옵션 리스트
+) : Serializable {
+    override fun toString(): String {
+        return "\n" +
+                "공시 체출월 : $dcls_month \n" +
+                "금융회사 코드 : $fin_co_no \n" +
+                "금융회사명 : $kor_co_nm \n" +
+                "금융상품 코드 : $fin_prdt_cd \n" +
+                "금융 상품명 : $fin_prdt_nm \n" +
+                "가입 방법 : $join_way \n" +
+                "만기후 이자율 : $mtrt_int \n" +
+                "우대조건 : $spcl_cnd \n" +
+                "가입제한 : $join_deny \n" +
+                "가입대상 : $join_member \n" +
+                "기타 유의사항 : $etc_note \n" +
+                "최고한도 : $max_limit \n" +
+                "공시 시작일 : $dcls_strt_day \n" +
+                "공시 종료일 : $dcls_end_day \n" +
+                "금융회사 제출일 : $fin_co_subm_day \n" +
+                "optionList : $optionListItem \n" +
+                ""
+    }
+}
+data class OptionListItem(
+        var intr_rate_type : String?,       // 저축 금리 유형
+        var intr_rate_type_nm : String?,    // 저축 금리 유형명
+        var save_trm : String?,             // 저축 기간 (단위 : 개월)
+        var intr_rate : String?,            // 저축 금리 (소수점 2자리)
+        var intr_rate2 : String?            // 최고 우대금리 (소수점 2자리)
+): Serializable {
+    override fun toString(): String {
+        return  "\n" +
+                "  저축 금리 유형 : $intr_rate_type \n" +
+                "  저축 금리 유형명 : $intr_rate_type_nm \n" +
+                "  저축 기간 (단위 : 개월) : $save_trm \n" +
+                "  저축 금리 (소수점 2자리) : $intr_rate \n" +
+                "  최고 우대금리 (소수점 2자리) : $intr_rate2"
+    }
+}
 
-//class BankDespositModelTree {
-//    var name: String? = null
-//    var messages: Array<String>
-//    var blog: String? = null
-//    var age: String? = null
-//
-//    override fun toString(): String {
-//        return "ClassPojo [name = $name, messages = $messages, blog = $blog, age = $age]"
-//    }
-//}
+
+
